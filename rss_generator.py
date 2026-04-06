2ca19a670b9cf05e2ad980f7627bd0b974069218
hs81-bit <harald.soffner@yahoo.de>
2026-04-06 14:06:15 +0200

    Update rss_generator.py

--- rss_generator.py
+++ rss_generator.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import re
 from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
 
@@ -8,10 +9,13 @@ URL = "https://www.meinschiff.com/presse/archiv"
 response = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
 html = response.text
 
-start = html.find('{"version"')
-end = html.rfind("</script>")
+# JSON Block per Regex extrahieren
+match = re.search(r'(\{"version".*?\})\s*</script>', html, re.DOTALL)
 
-json_text = html[start:end]
+if not match:
+    raise Exception("JSON Block nicht gefunden")
+
+json_text = match.group(1)
 data = json.loads(json_text)
 
 entries = data["content"]["slots"]["content"][1]["properties"]["entries"]
@@ -19,7 +23,7 @@ entries = data["content"]["slots"]["content"][1]["properties"]["entries"]
 fg = FeedGenerator()
 fg.title("Mein Schiff Pressemitteilungen")
 fg.link(href=URL)
-fg.description("Automatischer RSS Feed für Pressemitteilungen")
+fg.description("Automatischer RSS Feed")
 
 for item in entries[:30]:
     fe = fg.add_entry()
@@ -27,7 +31,7 @@ for item in entries[:30]:
     clean_text = BeautifulSoup(
         item["description"],
         "html.parser"
-    ).get_text()
+    ).get_text(" ", strip=True)
 
     title = clean_text[:120]
     link = "https://www.meinschiff.com" + item["link"]["href"]
@@ -38,4 +42,4 @@ for item in entries[:30]:
     fe.guid(link)
 
 fg.rss_file("docs/rss.xml")
-print("rss.xml erstellt")
+print("RSS erfolgreich erstellt")
